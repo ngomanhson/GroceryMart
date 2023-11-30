db4246b15e8e8ba612e6cab5ac3e563333bd73dc
ngomanhson <ngomanhson2004txpt@gmail.com>
2023-11-30 21:04:28 +0700

    Build frame Mega menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,64 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
 function load(selector, path) {
     const cached = localStorage.getItem(path);
     if (cached) {
-        document.querySelector(selector).innerHTML = cached;
+        $(selector).innerHTML = cached;
     }
 
     fetch(path)
         .then((res) => res.text())
         .then((html) => {
             if (html !== cached) {
-                document.querySelector(selector).innerHTML = html;
+                $(selector).innerHTML = html;
                 localStorage.setItem(path, html);
             }
+        })
+        .finally(() => {
+            window.dispatchEvent(new Event("template-loaded"));
         });
 }
+
+function isHidden(element) {
+    if (!element) return true;
+
+    if (window.getComputedStyle(element).display === "none") {
+        return true;
+    }
+
+    let parent = element.parentElement;
+    while (parent) {
+        if (window.getComputedStyle(parent).display === "none") {
+            return true;
+        }
+        parent = parent.parentElement;
+    }
+
+    return false;
+}
+
+function debounce(func, timeout = 300) {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            func.apply(this, args);
+        }, timeout);
+    };
+}
+
+const calArrowPos = debounce(() => {
+    if (isHidden($(".js-dropdown-list"))) return;
+
+    const items = $$(".js-dropdown-list > li");
+
+    items.forEach((item) => {
+        const arrowPos = item.offsetLeft + item.offsetWidth / 2;
+        item.style.setProperty("--arrow-left-pos", `${arrowPos}px`);
+    });
+});
+
+window.addEventListener("resize", calArrowPos);
+
+window.addEventListener("template-loaded", calArrowPos);
