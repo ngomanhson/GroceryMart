3564c644df52444d32375477883d87c81a690c2a
ngomanhson <ngomanhson2004txpt@gmail.com>
2023-12-02 02:19:22 +0700

    Fix sub menu scrollbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+/**
+ * Hàm tải template
+ *
+ * Cách dùng:
+ * <div id="parent"></div>
+ * <script>
+ *  load("#parent", "./path-to-template.html");
+ * </script>
+ */
 function load(selector, path) {
     const cached = localStorage.getItem(path);
     if (cached) {
@@ -20,6 +29,10 @@ function load(selector, path) {
         });
 }
 
+/**
+ * Hàm kiểm tra một phần tử
+ * có bị ẩn bởi display: none không
+ */
 function isHidden(element) {
     if (!element) return true;
 
@@ -38,6 +51,10 @@ function isHidden(element) {
     return false;
 }
 
+/**
+ * Hàm buộc một hành động phải đợi
+ * sau một khoảng thời gian mới được thực thi
+ */
 function debounce(func, timeout = 300) {
     let timer;
     return (...args) => {
@@ -48,6 +65,13 @@ function debounce(func, timeout = 300) {
     };
 }
 
+/**
+ * Hàm tính toán vị trí arrow cho dropdown
+ *
+ * Cách dùng:
+ * 1. Thêm class "js-dropdown-list" vào thẻ ul cấp 1
+ * 2. CSS "left" cho arrow qua biến "--arrow-left-pos"
+ */
 const calArrowPos = debounce(() => {
     if (isHidden($(".js-dropdown-list"))) return;
 
@@ -59,6 +83,52 @@ const calArrowPos = debounce(() => {
     });
 });
 
+// Tính toán lại vị trí arrow khi resize trình duyệt
 window.addEventListener("resize", calArrowPos);
 
+// Tính toán lại vị trí arrow sau khi tải template
 window.addEventListener("template-loaded", calArrowPos);
+
+/**
+ * Giữ active menu khi hover
+ *
+ * Cách dùng:
+ * 1. Thêm class "js-menu-list" vào thẻ ul menu chính
+ * 2. Thêm class "js-dropdown" vào class "dropdown" hiện tại
+ *  nếu muốn reset lại item active khi ẩn menu
+ */
+window.addEventListener("template-loaded", handleActiveMenu);
+
+function handleActiveMenu() {
+    const dropdowns = $$(".js-dropdown");
+    const menus = $$(".js-menu-list");
+    const activeClass = "menu-column__item--active";
+
+    const removeActive = (menu) => {
+        menu.querySelector(`.${activeClass}`)?.classList.remove(activeClass);
+    };
+
+    const init = () => {
+        menus.forEach((menu) => {
+            const items = menu.children;
+            if (!items.length) return;
+
+            removeActive(menu);
+            items[0].classList.add(activeClass);
+
+            Array.from(items).forEach((item) => {
+                item.onmouseenter = () => {
+                    if (window.innerWidth <= 991) return;
+                    removeActive(menu);
+                    item.classList.add(activeClass);
+                };
+            });
+        });
+    };
+
+    init();
+
+    dropdowns.forEach((dropdown) => {
+        dropdown.onmouseleave = () => init();
+    });
+}
